87976bbe53096aa1d045c3037d27136c6badc35b
Vishnu Nakade <vishnunakade@gmail.com>
2024-02-17 17:35:26 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import './App.css';
-import LoadingBar from 'react-top-loading-bar'
-
-import React, { Component } from 'react'
+import React from 'react';
 import NavBar from './components/NavBar';
 import News from './components/News';
+import LoadingBar from 'react-top-loading-bar';
 
-export default class App extends Component {
-  
-  render() {
-    return (
-      <div>
-       <LoadingBar
-        color='#f11946'
-        progress={10}
-        
-      />
-        <NavBar/>
-        <News/>
-      </div>
-    )
-  }
+
+
+export default function App() {
+  return (
+    <div>
+
+      <NavBar/>
+
+      <LoadingBar
+      color='#f11946'
+      progress={100}
+      
+    />
+      <News/>
+    </div>
+  )
 }
+
 
 
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,24 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
-
- 
-
-  render() {
-    let {title,description,imageUrl,newsUrl}=this.props;
-    return (
-      <div className='my-3'>
-            <div className="card" style={{width:"18rem"}}>
-            <img src={!imageUrl?"https://www.livemint.com/lm-img/img/2023/09/19/600x338/stock_market_holiday_on_Ganesh_Chaturthi_2023_1695085464149_1695085464303.jpg":imageUrl} className="card-img-top" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{description}</p>
-              <a href={newsUrl} target='blank' className="btn btn-dark btn-sm">Read More</a>
-            </div>
+export default function NewsItem(props) {
+  return (
+    <div className='my-3'>
+          <div className="card" style={{width:"18rem"}}>
+          <img src={!props.imageUrl?"https://www.livemint.com/lm-img/img/2023/09/19/600x338/stock_market_holiday_on_Ganesh_Chaturthi_2023_1695085464149_1695085464303.jpg":props.imageUrl} className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">{props.description}</p>
+            <a href={props.newsUrl} target='blank' className="btn btn-dark btn-sm">Read More</a>
           </div>
-      </div>
-    )
-  }
+        </div>
+    </div>
+  )
 }
-
-export default NewsItem
